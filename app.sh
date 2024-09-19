212edbd4950e185517462e6597eff9ae0486cfac
Hilmy Pratama <94878524+mxzyy@users.noreply.github.com>
2024-09-19 15:26:49 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,6 +6,15 @@ OUTPUT_FILE="result.json"
 
 # Fungsi untuk menampilkan bantuan
 show_help() {
+    cat << "EOF"
+______                      _     _____              _         
+|  _  \                    (_)   |_   _|            | |        
+| | | |___  _ __ ___   __ _ _ _ __ | |_ __ __ _  ___| | ___ __ 
+| | | / _ \| '_ ` _ \ / _` | | '_ \| | '__/ _` |/ __| |/ / '__|
+| |/ / (_) | | | | | | (_| | | | | | | | | (_| | (__|   <| |   
+|___/ \___/|_| |_| |_|\__,_|_|_| |_\_/_|  \__,_|\___|_|\_\_|    
+EOF
+    printf "%-30s\n" "-------------------------------------------------------------"
     echo "$PROGRAM_NAME - Cek status domain dan ekspor hasil ke JSON"
     echo "Usage:"
     echo "  -u <url>     Cek status satu domain"
